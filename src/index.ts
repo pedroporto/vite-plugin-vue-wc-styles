206fac2e3c4e0c70e2792ceb3dfa38964711c872
pedroporto <mail@pedroporto.com.pt>
2023-09-28 16:30:17 +0100

    feat: enabled HMR in wc styles plugin

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
-import { Plugin } from 'vite';
+import { Plugin, ViteDevServer } from 'vite';
+
+const invalidateModule = (server: ViteDevServer, moduleId: string) => {
+  const { moduleGraph, ws } = server;
+  const module = moduleGraph.getModuleById(moduleId);
+  if (module) {
+    moduleGraph.invalidateModule(module);
+    if (ws) {
+      ws.send({
+        type: 'full-reload',
+        path: '*',
+      });
+    }
+  }
+};
 
 /**
  * Vite Plugin to replace a placeholder with the bundled style of the custom element
  * @returns {Plugin}
  */
 const vueWcStyles = (): Plugin => {
+  let server: ViteDevServer;
+  let virtualStyles = '';
+  const moduleIds: string[] = [];
+
   return {
     name: 'vue-wc-styles',
     enforce: 'post',
@@ -33,6 +51,41 @@ const vueWcStyles = (): Plugin => {
           }
         }
       });
+    },
+    configureServer(_server) {
+      server = _server;
+    },
+    handleHotUpdate() {
+      moduleIds.forEach((id) => {
+        const module = server.moduleGraph.getModuleById(id);
+
+        if (module) {
+          server.reloadModule(module);
+        }
+      });
+    },
+    transform(code, id) {
+      if (server) {
+        if (id.includes('.scss')) {
+          const match = code.match(/const __vite__css = [`"]([^`"]*)[`"]/s);
+          if (match) {
+            const extractedCSS = match[1];
+            virtualStyles += extractedCSS;
+            if (!moduleIds.includes(id)) {
+              moduleIds.push(id);
+            }
+          }
+        }
+        if (code.includes('{{ CUSTOM_CSS_PLACEHOLDER }}')) {
+          code = code.replace('{{ CUSTOM_CSS_PLACEHOLDER }}', virtualStyles);
+          invalidateModule(server, id);
+          if (!moduleIds.includes(id)) {
+            moduleIds.push(id);
+          }
+        }
+      }
+
+      return code;
     },
   };
 };
