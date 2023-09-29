5b64b0511ee0497290b2f0b8681ed92b952ef296
pedroporto <mail@pedroporto.com.pt>
2023-09-29 10:51:15 +0100

    chore: bump

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Plugin } from 'vite';
-/**
- * Vite Plugin to replace a placeholder with the bundled style of the custom element
- * @returns {Plugin}
- */
-declare const vueWcStyles: () => Plugin;
-export default vueWcStyles;
